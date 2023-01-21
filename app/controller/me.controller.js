5f8fe03b15c561a8b0163c53a41bf54d263ed78b
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2023-01-21 16:26:19 +0200

    update

--- app/controller/me.controller.js
+++ app/controller/me.controller.js
@@ -3,6 +3,7 @@ const { validatePassword } = require("../helper/crypt.helper");
 const sendEmail = require("../helper/email.helper");
 const Helper = require("../helper/helper");
 const ModelHelper = require("../helper/model.helper");
+const bcryptjs = require("bcryptjs");
 class MeController {
 	static me = Helper.catchAsyncError((req, res, next) => {
 		Helper.resHandler(res, 200, true, req.user, "user fetched");
@@ -11,13 +12,12 @@ class MeController {
 	static changePassword = Helper.catchAsyncError(async (req, res, next) => {
 		if (req.body.password == req.body.newPassword)
 			throw new Error("old password and new the same");
-		const user = await  UserModel.findById({ _id: req.user._id }).select("+password")
+		const user = await  UserModel.findById({ _id: req.user._id }).select("+password");
 		if (!user) throw new AppError("there no user with this email ");
 		const validPassword = await validatePassword(
 			req.body.password,
 			user.password,
 		);
-		console.log(validPassword);
 		if (!validPassword) throw new Error("invalid password");
 		user.password = req.body.newPassword;
 		await user.save();
@@ -26,7 +26,7 @@ class MeController {
 			subject: "Your password Is changed",
 			text: `Your password Is changed`,
 		});
-		Helper.resHandler(res, 200, true, currentUser, "user password updated");
+		Helper.resHandler(res, 200, true, req.user, "user password updated");
 	});
 	static changeEmail = Helper.catchAsyncError(async (req, res, next) => {
 		const user = await ModelHelper.findOne(UserModel, { _id: req.user._id });
@@ -41,7 +41,7 @@ class MeController {
 		await sendEmail({
 			email: user.email,
 			subject: "Your Email Is changed",
-			text: `/${user._id}/active`,
+			text: `/${user._id}/acGETtive`,
 		});
 
 		Helper.resHandler(res, 200, true, currentUser, "user Email updated");
@@ -53,13 +53,14 @@ class MeController {
 		if (req.body.email)
 			throw new Error("To Update email You Must Using me/updateEmail");
 		const newUserData = { ...req.user._doc, ...req.body };
-		const Building = await ModelHelper.updateOne(
+		const user = await ModelHelper.updateOne(
 			UserModel,
 			{
 				_id: req.user._id,
 			},
 			newUserData,
 		);
+		req.user=user;
 		Helper.resHandler(res, 200, true, req.user, "user updated");
 	});
 	static deleteMe = Helper.catchAsyncError(async (req, res, next) => {
@@ -68,14 +69,17 @@ class MeController {
 		Helper.resHandler(res, 200, true, null, "user deleted ");
 	});
 	static changeProfileImage = Helper.catchAsyncError(async (req, res, next) => {
-		req.user.profileImage = req.file.filename;
-		await req.user.save();
-		const user = { ...req.user._doc };
-		delete user.role;
-		delete user.tokens;
+		const user = await ModelHelper.updateOne(
+			UserModel,
+			{
+				_id: req.user._id,
+			},
+			{profileImage:req.file.filename},
+		);
+		req.user=user;
 		res.status(200).json({
 			status: "success",
-			data: user,
+			data: req.user,
 		});
 	});
 }
